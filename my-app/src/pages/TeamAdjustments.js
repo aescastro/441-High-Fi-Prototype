63c4a0331ce6c47eb5dfaf7ed528a8be66ec7593
TylerConnelly <70350902+TylerConnelly@users.noreply.github.com>
2023-12-02 12:45:07 -0800

    Update TeamAdjustments.js

--- my-app/src/pages/TeamAdjustments.js
+++ my-app/src/pages/TeamAdjustments.js
@@ -48,44 +48,45 @@ function MyDragAndDropComponent() {
       [
         {name:"Zack",yearLevel:2,yoe:7,skills:["HTML","CSS","Redux","Express"]},
         {name:"Derek",yearLevel:2,yoe:6,skills:["JavaScript","Java","Git","Vue.js"]},
-        {name:"Liam",yearLevel:2,yoe:5,skills:["Java","SQL","React","AWS"]},
-        {name:"Leo",yearLevel:4,yoe:5,skills:["React","Node.js","MongoDB","JIRA"]},
-        {name:"Wendy",yearLevel:3,yoe:8,skills:["Java","SQL","AWS","Python"]}	  
+        {name:"Liam",yearLevel:3,yoe:5,skills:["Java","SQL","React","AWS"]},
+      {name:"Leo",yearLevel:4,yoe:5,skills:["React","Node.js","MongoDB","JIRA"]},
+        {name:"Wendy",yearLevel:1,yoe:8,skills:["Java","SQL","AWS","Python"]}	  
       ],
       [
-        {name:"Alice",yearLevel:1,yoe:6,skills:["JavaScript","React","CSS","MongoDB"]},
+        {name:"Alice",yearLevel:4,yoe:6,skills:["JavaScript","React","CSS","MongoDB"]},
         {name:"Emma",yearLevel:1,yoe:7,skills:["React","Node.js","MongoDB","Git"]},
         {name:"Ivy",yearLevel:1,yoe:8,skills:["Python","Java","React","Angular"]},
-        {name:"Mia",yearLevel:1,yoe:5,skills:["HTML","CSS","AWS","Scrum"]},
-        {name:"Quinn",yearLevel:1,yoe:5,skills:["JavaScript","HTML","CSS","Webpack"]}
+        {name:"Mia",yearLevel:3,yoe:5,skills:["HTML","CSS","AWS","Scrum"]},
+        {name:"Quinn",yearLevel:2,yoe:5,skills:["JavaScript","HTML","CSS","Webpack"]}
       ],
       [
-        {name:"Rachel",yearLevel:2,yoe:7,skills:["Java","SQL","React","JIRA"]},
-        {name:"Tom",yearLevel:2,yoe:6,skills:["HTML","CSS","Redux","Express"]},
-        {name:"Xavier",yearLevel:2,yoe:8,skills:["JavaScript","Java","Git","Vue.js"]},
-        {name:"Yasmine",yearLevel:3,yoe:5,skills:["React","Node.js","Redux","Express"]},
-        {name:"Zane",yearLevel:4,yoe:5,skills:["Python","Docker","MongoDB","Angular"]}
+        {name:"Rachel",yearLevel:3,yoe:7,skills:["Java","SQL","React","JIRA"]},
+        {name:"Tom",yearLevel:3,yoe:6,skills:["HTML","CSS","Redux","Express"]},
+        {name:"Xavier",yearLevel:1,yoe:8,skills:["JavaScript","Java","Git","Vue.js"]},
+        {name:"Yasmine",yearLevel:4,yoe:5,skills:["React","Node.js","Redux","Express"]},
+        {name:"Zane",yearLevel:2,yoe:5,skills:["Python","Docker","MongoDB","Angular"]}
       ],
       [
-        {name:"Charlie",yearLevel:3,yoe:5,skills:["HTML","CSS","Node.js","Express"]},
-        {name:"Grace",yearLevel:3,yoe:6,skills:["Java","SQL","AWS","Vue.js"]},
-        {name:"Katie",yearLevel:3,yoe:7,skills:["JavaScript","Java","SQL","Docker"]},
+        {name:"Charlie",yearLevel:4,yoe:5,skills:["HTML","CSS","Node.js","Express"]},
+        {name:"Grace",yearLevel:1,yoe:6,skills:["Java","SQL","AWS","Vue.js"]},
+        {name:"Katie",yearLevel:2,yoe:7,skills:["JavaScript","Java","SQL","Docker"]},
         {name:"Olivia",yearLevel:3,yoe:8,skills:["React","Node.js","Redux","Express"]},
-        {name:"Sam",yearLevel:3,yoe:5,skills:["Python","Node.js","Redux","Scrum"]}
+        {name:"Sam",yearLevel:2,yoe:5,skills:["Python","Node.js","Redux","Scrum"]}
       ],
       [
-        {name:"David",yearLevel:4,yoe:7,skills:["Python","Docker","Redux","RESTful API"]},
-        {name:"Henry",yearLevel:4,yoe:8,skills:["React","Node.js","Java","Webpack"]},
+         {name:"Henry",yearLevel:4,yoe:8,skills:["React","Node.js","Java","Webpack"]},
         {name:"Vince",yearLevel:2,yoe:8,skills:["JavaScript","HTML","CSS"]},
-        {name:"Peter",yearLevel:4,yoe:6,skills:["Python","Docker","MongoDB","Angular"]},
-        {name:"Tina",yearLevel:4,yoe:5,skills:["HTML","CSS","Docker","RESTful API"]}
+        {name:"Peter",yearLevel:3,yoe:6,skills:["Python","Docker","MongoDB","Angular"]},
+        {name:"Tina",yearLevel:1,yoe:5,skills:["HTML","CSS","Docker","RESTful API"]},
+      {name:"Gina",yearLevel:3,yoe:5,skills:["HTML","CSS","Docker","RESTful API"]}
+  
       ],
       [
+      {name:"David",yearLevel:4,yoe:7,skills:["Python","Docker","Redux","RESTful API"]},
         {name:"Uma",yearLevel:1,yoe:6,skills:["React","Node.js","MongoDB","Git"]},
-        {name:"Ingrid",yearLevel:3,yoe:7,skills:["Python","Node.js","Redux","Scrum"]},
+        {name:"Ingrid",yearLevel:1,yoe:7,skills:["Python","Node.js","Redux","Scrum"]},
         {name:"Hank",yearLevel:2,yoe:5,skills:["Java","SQL","React","JIRA"]},
-        {name:"Gina",yearLevel:1,yoe:5,skills:["HTML","CSS","Docker","RESTful API"]},
-        {name:"Frank",yearLevel:2,yoe:5,skills:["JavaScript","HTML","CSS"]}
+        {name:"Frank",yearLevel:3,yoe:5,skills:["JavaScript","HTML","CSS"]}
       ]
     ];
   };
